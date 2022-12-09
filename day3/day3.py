135c2f9524ec65cbf2db7c9179e093773d3f066d
Adam Cordingley <cordingleyadam19+github@gmail.com>
2022-12-08 22:10:01 -0500

    Complete Day 3

--- day3/day3.py
+++ day3/day3.py
@@ -13,7 +13,13 @@ def decode_priority(letter):
 
     return ind + 1
 #------------------------------------------------------------------------------
-
+def find_common_letters(a, b):
+    common = []
+    for letter in a:
+        if letter in b:
+            common.append(letter)
+    return common
+#------------------------------------------------------------------------------
 
 if __name__ == "__main__":
     file_list = os.listdir()
@@ -27,16 +33,43 @@ if __name__ == "__main__":
 
     f = open(fname, 'r')
 
+    priority_sum = 0
+
     for line in f:
         all_items = line.strip()
-        comp_1 = []
-        comp_2 = []
-        for i in range(0, len(all_items)):
-            if i < len(all_items)/2:
-                comp_1.append(all_items[i])
-            else:
-                comp_2.append(all_items[i])
+        halfway = int(len(all_items)/2)
+        comp_1 = all_items[0:halfway]
+        comp_2 = all_items[halfway:len(all_items)]
         
+        common_letter = ''
+        for letter in comp_1:
+            if letter in comp_2:
+                common_letter = letter
+                break
+        priority_sum += decode_priority(common_letter)
+        
+    print("Priority-Sum: {}".format(priority_sum))
+    f.close()
 
+    
+    f = open(fname, 'r')
+
+    line_counter = 0
+    priority_sum = 0
+    group = []
+    for line in f:
+        all_items = line.strip()
+        group.append(all_items)
+        line_counter += 1
+        if line_counter % 3 == 0:
+            # analyze this group of elves
+            badge = ''
+            for letter in group[0]:
+                if letter in group[1] and letter in group[2]:
+                    badge = letter
+            priority_sum += decode_priority(badge)
+            group = []
+    
+    print("Priority-Sum for badges: {}".format(priority_sum))
 
     f.close()
